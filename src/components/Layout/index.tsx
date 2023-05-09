b5217177e85a600dadecca8f47263f2bde42fcbe
Alan Silva <alanfranciscos@gmail.com>
2023-05-09 20:07:23 -0300

    feat: write components tests - add test in the layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,14 +6,12 @@ import { Container, Content, PageContainer } from "./styles";
 export const Layout = ({ children }: { children: any }) => {
   const [sideBarIsOpen, setsideBarIsOpen] = useState(undefined);
 
-  console.log(sideBarIsOpen);
-
   return (
-    <Container>
-      <Header setsideBarIsOpen={setsideBarIsOpen} />
-      <PageContainer>
-        <SideBar sideBarIsOpen={sideBarIsOpen} />
-        <Content>{children}</Content>
+    <Container data-testid="layout">
+      <Header setsideBarIsOpen={setsideBarIsOpen}/>
+      <PageContainer data-testid="layout-content">
+        <SideBar sideBarIsOpen={sideBarIsOpen}/>
+        <Content data-testid="layout-content-content">{children}</Content>
       </PageContainer>
     </Container>
   );
