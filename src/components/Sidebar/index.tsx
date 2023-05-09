6dfb0fdec8c11bc6dfca1020c7046ef558d6c2db
Alan Silva <alanfranciscos@gmail.com>
2023-05-09 20:48:27 -0300

    feat: write components tests - add tests in sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,20 +7,20 @@ export const SideBar = ({
 }) => {
   return (
     <Container notClosed={sideBarIsOpen} data-testid="sidebar">
-      <h2>GESTÃO DE CLIENTES</h2>
-      <ul>
+      <h2 data-testid="sidebar-title">GESTÃO DE CLIENTES</h2>
+      <ul data-testid="sidebar-list">
         <li>Lista de clientes</li>
         <li>Cadastrar cliente</li>
       </ul>
 
-      <h2>GESTÃO DE VENDAS</h2>
-      <ul>
+      <h2 data-testid="sidebar-title">GESTÃO DE VENDAS</h2>
+      <ul data-testid="sidebar-list">
         <li>Lista de vendas</li>
         <li>Cadastrar venda</li>
       </ul>
 
-      <h2>RELATÓRIOS</h2>
-      <ul>
+      <h2 data-testid="sidebar-title">RELATÓRIOS</h2>
+      <ul data-testid="sidebar-list">
         <li>Relatórios</li>
       </ul>
     </Container>
